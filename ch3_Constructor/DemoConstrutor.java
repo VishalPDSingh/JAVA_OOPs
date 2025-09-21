ea77eaf7671346f3a187f97c7a8389bf73f2fd5c
VishalPDSingh <vishalprasadsingh52@gmail.com>
2025-09-21 06:07:38 +0530

    first commit

---
+++ ch3_Constructor/DemoConstrutor.java
@@ -0,0 +1,45 @@
+package ch3_Constructor;
+
+
+class Customer
+{
+    private int cId;
+    private String cName;
+    private long cNum;
+
+    public void setData(int cId, String cName, long cNum)
+    {
+        this.cId = cId;
+        this.cName = cName;
+        this.cNum = cNum;
+    }
+
+    public int getId()
+    {
+        return cId;
+    }
+
+    public String getName()
+    {
+        return cName;
+    }
+
+    public long getNum()
+    {
+        return cNum;
+    }
+}
+
+
+
+
+public class DemoConstrutor {
+    public static void main(String[] args) {
+        
+        Customer c1 = new Customer();
+        c1.setData(10, "aaaa", 132654);
+        System.out.println(c1.getId());
+        System.out.println(c1.getName());
+        System.out.println(c1.getNum());
+    }
+}
